5497fb7b8c3b18b282f3ce6c05af67d9c370dd55
Your Name <you@example.com>
2024-12-28 19:16:19 +0900

    localhost:8080の起動に成功するまで

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # airflow-local-test
+ローカルでテストする用のリポジトリ  
+
+### 実行手順
+
+- イメージ取得  
+docker pull apache/airflow:2.10.4-python3.12
+- 権限変更  
+chmod +x run_airflow.sh
+- imageの実行  
+./run_airflow.sh
---
+++ run_airflow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 環境変数の設定
+EXECUTOR="SequentialExecutor"
+DAGS_FOLDER="/opt/airflow/dags"
+SQL_ALCHEMY_CONN="sqlite:////opt/airflow/airflow.db"
+CONSTRAINTS_URL="https://raw.githubusercontent.com/apache/airflow/constraints-2.10.4/constraints-3.12.txt"
+
+# Docker Run コマンド
+docker run -d -p 8080:8080 \
+  -e AIRFLOW__CORE__EXECUTOR=$EXECUTOR \
+  -e AIRFLOW__CORE__DAGS_FOLDER=$DAGS_FOLDER \
+  -e AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$SQL_ALCHEMY_CONN \
+  -e AIRFLOW_CONSTRAINTS=$CONSTRAINTS_URL \
+  -v $(pwd)/dags:/opt/airflow/dags \
+  apache/airflow:2.10.4-python3.12 bash -c "airflow db init && airflow webserver"
